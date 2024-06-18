58f337fcdeedeaa7b6aca61ad1b5ff398ed9c90c
Thinh Pham <thinhpham2405@gmail.com>
2024-06-18 12:34:46 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header  from "./components/Header"
+import Home from "./pages/Home";
 
 function App() {
 
   return(
-    <Header/>
+    <>
+      <Header/>  
+      <Home/>
+    </>
   );
 }
 
